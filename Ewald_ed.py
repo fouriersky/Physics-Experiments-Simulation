d15e42c274fda1fa8ea17ca44b4122921c04c399
fouriersky <163293907+fouriersky@users.noreply.github.com>
2025-05-02 00:13:57 +0800

    Add files via upload

---
+++ Ewald_ed.py
@@ -0,0 +1,39 @@
+import numpy as np
+from ase.io import read
+from ewald import ewaldsum
+
+if __name__ == '__main__':
+
+    crystals = [
+        'NaCl.vasp',
+        'CsCl.vasp',
+        'ZnO-Hex.vasp',
+        'ZnO-Cub.vasp',
+        'TiO2.vasp',
+        'CaF2.vasp',
+    ]
+
+    ZZ = {
+        'Na':  1, 'Ca':  2,
+        'Cl': -1,  'F': -1,
+        'Cs':  1,
+        'Ti':  4,
+        'Zn':  2,
+         'O': -2,
+    }
+
+    print('-' * 41)
+    print(f'{"Crystal":>9s} | Ref Atom | {"Madelung Constant":>18s}')
+    print('-' * 41)
+
+    for crys in crystals:
+        atoms = read(crys)
+        esum = ewaldsum(atoms, ZZ) 
+
+        # print(esum.get_ewaldsum())
+        M = esum.get_madelung()
+        C = crys.replace('.vasp', '')
+        R = atoms.get_chemical_symbols()[0]
+        print(f'{C:>9s} | {R:^8s} | {M:18.12f}')
+
+    print('-' * 41)
